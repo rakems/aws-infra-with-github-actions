fdc2acf05ba2fb4a3fb57dde962a4c2d6055e6b9
Rakesh <rakesh.srikanth@knightec.se>
2024-10-23 17:11:02 +0200

    chore: Update IAM policy name in main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ resource "aws_iam_role" "ec2_role_test" {
   })
 }
 
-resource "aws_iam_policy" "ec2_policy" {
-  name = "ec2_policy"
+resource "aws_iam_policy" "ec2_policy_test" {
+  name = "ec2_policy_test"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -36,7 +36,7 @@ resource "aws_iam_policy" "ec2_policy" {
 
 resource "aws_iam_role_policy_attachment" "ec2_attach" {
   role       = aws_iam_role.ec2_role_test.name
-  policy_arn = aws_iam_policy.ec2_policy.arn
+  policy_arn = aws_iam_policy.ec2_policy_test.arn
 }
 
 resource "aws_instance" "example" {
