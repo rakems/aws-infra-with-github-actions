6448bd8e54ecdfcacc0db3713f0615eeccbe6388
Rakesh <rakesh.srikanth@knightec.se>
2024-10-24 10:45:53 +0200

    chore: Update AWS resources configuration in main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_iam_role" "ec2_role_test" {
   name = "ec2_role_test"
 
