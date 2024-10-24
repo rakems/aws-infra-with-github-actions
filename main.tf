158e696b9040a6ee17fb872ab6df57e41feb41c2
Rakesh <rakesh.srikanth@knightec.se>
2024-10-24 15:53:40 +0200

    chore: Update AWS resources configuration in main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
+# IAM Role and Policy
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
 resource "aws_iam_role" "ec2_role_test" {
   name = "ec2_role_test"
 
@@ -39,7 +27,25 @@ resource "aws_iam_policy" "ec2_policy_test" {
           "ec2:TerminateInstances",
           "ec2:DescribeInstances",
           "ec2:CreateTags",
-          "ec2:DescribeImages"
+          "ec2:DescribeImages",
+          "ec2:CreateVpc",
+          "ec2:DeleteVpc",
+          "ec2:DescribeVpcs",
+          "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:DescribeSubnets",
+          "ec2:CreateInternetGateway",
+          "ec2:DeleteInternetGateway",
+          "ec2:AttachInternetGateway",
+          "ec2:DetachInternetGateway",
+          "ec2:CreateRouteTable",
+          "ec2:DeleteRouteTable",
+          "ec2:AssociateRouteTable",
+          "ec2:DisassociateRouteTable",
+          "ec2:CreateSecurityGroup",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:AuthorizeSecurityGroupEgress",
+          "ec2:DeleteSecurityGroup"
         ],
         Effect   = "Allow",
         Resource = "*"
@@ -53,26 +59,96 @@ resource "aws_iam_role_policy_attachment" "ec2_attach" {
   policy_arn = aws_iam_policy.ec2_policy_test.arn
 }
 
-data "aws-vpc" "my_vpc" {
+# Create a new VPC
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "my_vpc"
   }
 }
 
-data "aws_subnet_ids" "my_subnet" {
+# Create a new Subnet in the VPC
+resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-north-1a"
+  tags = {
+    Name = "my_subnet"
+  }
 }
 
-resource "aws_instance" "example" {
-  ami                  = "ami-06b21ccaeff8cd686"
-  instance_type        = "t2.micro"
-  subnet_id            = aws_subnet.my_subnet.id
-  iam_instance_profile = aws_iam_role.ec2_role_test.name
+# Create an Internet Gateway for VPC
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Create a Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
 
   tags = {
-    Name = "example-instance"
+    Name = "my_route_table"
+  }
+}
+
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "my_route_table_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Create a Security Group
+resource "aws_security_group" "my_security_group" {
+  name        = "my_security_group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my_security_group"
+  }
+}
+
+# Create an IAM Instance Profile for EC2
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile_test"
+  role = aws_iam_role.ec2_role_test.name
+}
+
+# Create the EC2 Instance
+resource "aws_instance" "sample_ec2" {
+  ami                    = "ami-02db68a01488594c5" # Update the AMI as per your requirement
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name # Use the instance profile here
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+
+  tags = {
+    Name = "sample_ec2-instance"
   }
 }
